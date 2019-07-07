757af263252ce8b18e8afebd8c449e699bb9fb06
Vinicius Souza <viniciusls22@gmail.com>
2019-07-07 09:27:48 -0300

    PSD A2: Corrige simulacao filtro 3

--- PSD1/A2/filtro3/projeto_hdl/hdlsrc/filtro3_iir_sim_r2015a/Filtro_IIR_BP_20bits_signed_compile.do
+++
@@ -1,3 +0,0 @@
-vlib work
-vcom  filter.vhd
-vcom  Filtro_IIR_BP_20bits_signed.vhd
--- PSD1/A2/filtro3/projeto_hdl/hdlsrc/filtro3_iir_sim_r2015a/html/filtro3_iir_sim_r2015a/filtro3_iir_sim_r2015a_sid_map.js
+++
@@ -1,9 +0,0 @@
-function RTW_SidParentMap() {
-    this.sidParentMap = new Array();
-    this.sidParentMap["filtro3_iir_sim_r2015a:206"] = "filtro3_iir_sim_r2015a:205";
-    this.sidParentMap["filtro3_iir_sim_r2015a:207"] = "filtro3_iir_sim_r2015a:205";
-    this.sidParentMap["filtro3_iir_sim_r2015a:208"] = "filtro3_iir_sim_r2015a:205";
-    this.sidParentMap["filtro3_iir_sim_r2015a:209"] = "filtro3_iir_sim_r2015a:205";
-    this.getParentSid = function(sid) { return this.sidParentMap[sid];}
-}
-    RTW_SidParentMap.instance = new RTW_SidParentMap();
